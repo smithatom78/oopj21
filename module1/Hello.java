b17bf95bee6af583012cc872e769bc838802f17d
Smitha Jacob <smithatom78@gmail.com>
2021-12-19 17:25:58 +0530

    Add files via upload

---
+++ module1/Hello.java
@@ -0,0 +1,16 @@
+package module1;
+/**
+* The HelloJava program implements an application that
+* simply displays "Hello Java!" to the standard output.
+*
+* @author  Smitha Jacob
+* @version 1.0
+* @since   2021-11-14 
+*/
+public class Hello {
+	public static void main(String[] args) {
+		//Prints Hello, Java! on standard output.
+		System.out.println("Hello Java!");
+		
+	}
+}
